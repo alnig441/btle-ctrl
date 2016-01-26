9b7cf4b525d6c1605b28fc4db58971139eb0f711
alnig441 <alnig441@gmail.com>
2016-01-26 17:14:00 -0600

    panel light switch function updated

--- routes/panel.js
+++ routes/panel.js
@@ -27,48 +27,57 @@ router.get('/', function(req, res, error){
             res.send(device);
         })
 
-        //res.send(user);
     })
-
-
-   //res.send('from panel get');
 
 });
 
-router.put('/', function(req, res, error) {
+router.put('/', function(req, res, error){
 
-    console.log('right here: ', req.body);
+    console.log('in panel put: ', req.body);
 
-    console.log(req.body.device_on === true);
+    var turnOn = '58010301ff00ffffff';
+    var turnOff = '58010301ff00000000';
+
+    var arr = [
+        '-i',
+        'hci1',
+        '-b',
+        req.body.mac,
+        '--char-write',
+        '-a',
+        '0x0028',
+        '-n'
+    ];
 
     if(req.body.device_on === true){
 
+        arr.push(turnOff);
+        console.log(arr);
 
-        var child = spawn('sudo', ['gatttool', '-i', 'hci1', '-b', req.body.device.mac, '--char-write', '-a', '0x0028', '-n', '58010301ff00000000']);
+        pg.connect(connectionString, function(err, client, done){
+
+            var query = client.query("UPDATE devices SET device_on=false where mac='" + req.body.mac + "'", function(error, result){
+                if(error){console.log('there was an error ', error.detail);}
+            })
+
+            query.on('end',function(result){
+                client.end();
+                res.send(result);
+            })
+
+        });
+
+        var child = spawn('gatttool', arr);
 
         child.stdout.on('data', function(data){
 
             res.send(data);
 
-            //child.kill();
+            child.kill();
         });
 
         child.on('exit', function(code){
             console.log('spawned process ended on exit code: ', code);
-        });
-
-        pg.connect(connectionString, function(err, client, done){
-            if(err){console.log(err);}
-
-            var query = client.query("UPDATE devices SET device_on=false WHERE mac='" + req.body.mac + "'", function(error, result){
-                if(error){console.log(error.detail);}
-            });
-
-            query.on('end', function(result){
-                client.end();
-                res.send(result);
-
-            });
         });
 
 
@@ -76,38 +85,39 @@ router.put('/', function(req, res, error) {
 
     if(req.body.device_on === false){
 
-        var child = spawn('sudo', ['gatttool', '-i', 'hci1', '-b', req.body.device.mac, '--char-write', '-a', '0x0028', '-n', '58010301ff00ffffff']);
+        arr.push(turnOn);
+
+        pg.connect(connectionString, function(err, client, done){
+
+            var query = client.query("UPDATE devices SET device_on=true where mac='" + req.body.mac + "'", function(error, result){
+                if(error){console.log('there was an error ', error.detail);}
+            })
+
+            query.on('end',function(result){
+                client.end();
+                res.send(result);
+            })
+
+        });
+
+        var child = spawn('gatttool', arr);
 
         child.stdout.on('data', function(data){
 
             res.send(data);
 
-            //child.kill();
+            child.kill();
         });
 
         child.on('exit', function(code){
             console.log('spawned process ended on exit code: ', code);
         });
 
-        pg.connect(connectionString, function(err, client, done){
-            if(err){console.log(err);}
 
-            var query = client.query("UPDATE devices SET device_on=true WHERE mac='" + req.body.mac + "'", function(error, result){
-                if(error){console.log(error.detail);}
-            });
-
-            query.on('end', function(result){
-                client.end();
-                res.send(result);
-
-            });
-        });
-
-
-    };
-
+    }
 
 
 });
+
 
 module.exports = router;
