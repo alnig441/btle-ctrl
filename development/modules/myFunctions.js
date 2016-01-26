f8e4c42940d4d6292deea44ca708e7ff641aa89f
alnig441 <alnig441@gmail.com>
2016-01-25 19:42:21 -0600

    cleanArray() clean up ..

--- development/modules/myFunctions.js
+++ development/modules/myFunctions.js
@@ -1,37 +1,32 @@
 var call = {
     cleanArray: function(array){
-        var y = [];
-        var z = [];
-        var tmp = [];
         var x = array.toString().replace(/\s/g, ',');
 
         array = x.split(',');
+        x = [];
 
         array.forEach(function(elem, ind, arr){
             if(elem.length == 17){
-                tmp.push(elem);
+                elem = {mac: elem};
+                x.push(elem);
             }
         });
 
-        array = tmp;
+        array = x;
+        x = [];
 
-        array.push('zzzzzzzzzzzzzzz');
+        array.push({mac: 'zzzzzzzzzzzzzzz'});
 
         array.sort().reduce(function(prev, curr, index, array){
             prev = array[index -1];
 
-            if(prev != curr){
-                y.push(prev);
+            if(prev.mac != curr.mac){
+                x.push(prev);
             }
 
         });
 
-        y.forEach(function(elem, index, arr){
-            elem = {mac: elem};
-            z.push(elem);
-        });
-
-        array = z;
+        array = x;
 
         return array;
     }
