3e08d8fea97f51cf358ecf69c6953fa881b1fb99
alnig441 <alnig441@gmail.com>
2016-01-27 11:02:52 -0600

    panel route refactored

--- routes/panel.js
+++ routes/panel.js
@@ -35,61 +35,58 @@ router.put('/', function(req, res, error){
 
     console.log('in panel put: ', req.body);
 
-    var turnOn = '58010301ff00ffffff';
-    var turnOff = '58010301ff00000000';
-
-    var arr = [
-        '-i',
-        'hci1',
-        '-b',
-        req.body.mac,
-        '--char-write',
-        '-a',
-        '0x0028',
-        '-n'
-    ];
-
-    if(req.body.device_on === true){
-
-        arr.push(turnOff);
-        console.log(arr);
-
-        pg.connect(connectionString, function(err, client, done){
-
-            var query = client.query("UPDATE devices SET device_on=false where mac='" + req.body.mac + "'", function(error, result){
-                if(error){console.log('there was an error ', error.detail);}
-            })
-
-            query.on('end',function(result){
-                client.end();
-                res.send(result);
-            })
-
-        });
-
-        var child = spawn('gatttool', arr);
-
-        child.stdout.on('data', function(data){
-
-            res.send(data);
-
-            child.kill();
-        });
-
-        child.on('exit', function(code){
-            console.log('spawned process ended on exit code: ', code);
-        });
-
-
+    var flipSwitch = {
+        on: '58010301ff00ffffff',
+        off: '58010301ff00000000',
+        gattArgs: [
+            '-i',
+            'hci1',
+            '-b',
+            req.body.mac,
+            '--char-write',
+            '-a',
+            '0x0028',
+            '-n'
+        ],
+        state: req.body.device_on
     };
 
-    if(req.body.device_on === false){
+    //var arr = [
+    //    '-i',
+    //    'hci1',
+    //    '-b',
+    //    req.body.mac,
+    //    '--char-write',
+    //    '-a',
+    //    '0x0028',
+    //    '-n'
+    //];
 
-        arr.push(turnOn);
+    //var flip;
+
+    switch (req.body.device_on) {
+        case true:
+            flipSwitch.gattArgs.push(flipSwitch.off);
+            flipSwitch.state = false;
+            break;
+        case false:
+            flipSwitch.gattArgs.push(flipSwitch.on);
+            flipSwitch.state = true;
+            break;
+    };
+
+    //var turnOn = '58010301ff00ffffff';
+    //var turnOff = '58010301ff00000000';
+
+
+    //if(req.body.device_on === true){
+
+        //arr.push(turnOff);
+        console.log(flipSwitch.gattArgs, flipSwitch.state);
 
         pg.connect(connectionString, function(err, client, done){
 
-            var query = client.query("UPDATE devices SET device_on=true where mac='" + req.body.mac + "'", function(error, result){
+            var query = client.query("UPDATE devices SET device_on='" + flipSwitch.state + "' where mac='" + req.body.mac + "'", function(error, result){
                 if(error){console.log('there was an error ', error.detail);}
             })
 
@@ -100,7 +97,7 @@ router.put('/', function(req, res, error){
 
         });
 
-        var child = spawn('gatttool', arr);
+        var child = spawn('gatttool', flipSwitch.gattArgs);
 
         child.stdout.on('data', function(data){
 
@@ -114,9 +111,42 @@ router.put('/', function(req, res, error){
         });
 
 
-    }
+    //};
 
-
+//    if(req.body.device_on === false){
+//
+//        arr.push(turnOn);
+//
+//        pg.connect(connectionString, function(err, client, done){
+//
+//            var query = client.query("UPDATE devices SET device_on=true where mac='" + req.body.mac + "'", function(error, result){
+//                if(error){console.log('there was an error ', error.detail);}
+//            })
+//
+//            query.on('end',function(result){
+//                client.end();
+//                res.send(result);
+//            })
+//
+//        });
+//
+//        var child = spawn('gatttool', arr);
+//
+//        child.stdout.on('data', function(data){
+//
+//            res.send(data);
+//
+//            child.kill();
+//        });
+//
+//        child.on('exit', function(code){
+//            console.log('spawned process ended on exit code: ', code);
+//        });
+//
+//
+//    }
+//
+//
 });
 
 
