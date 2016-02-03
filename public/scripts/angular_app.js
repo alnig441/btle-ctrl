fb008dfe39ce220e9ab7d699a060946ae1615653
alnig441 <alnig441@gmail.com>
2016-02-03 09:27:02 -0600

    login dialog complete

--- public/scripts/angular_app.js
+++ public/scripts/angular_app.js
@@ -30,53 +30,15 @@ app.config(function($routeProvider, $locationProvider, $mdThemingProvider){
 
 ;app.controller('loginCtrl',['$scope', '$rootScope', '$http', '$location', '$mdDialog', '$mdMedia', function ($scope, $rootScope, $http, $location, $mdDialog, $mdMedia){
 
-    console.log('in loginCtrl');
-
     $rootScope.template = '/views/panel.html';
 
     $rootScope.edinaa = {};
-
-    $rootScope.form = {};
-
-    $scope.submit = function(){
-
-        $http.post('/login/authenticate', $scope.form)
-            .then(function(response){
-                if(response.data === true){
-                    $location.path('/admin');
-                }
-                else{$location.path('/default');}
-            });
-    };
 
     $scope.logout = function(){
         $location.path('/default');
     };
 
-    //$scope.isOpen = false;
-    //$scope.demo = {
-    //    isOpen: false,
-    //    count: 0,
-    //    selectedDirection: 'right'
-    //};
-
-
     $scope.customFullscreen = $mdMedia('xs') || $mdMedia('sm');
-    $scope.showAlert = function(ev) {
-        // Appending dialog to document.body to cover sidenav in docs app
-        // Modal dialogs should fully cover application
-        // to prevent interaction outside of dialog
-        $mdDialog.show(
-            $mdDialog.alert()
-                .parent(angular.element(document.querySelector('#popupContainer')))
-                .clickOutsideToClose(true)
-                .title('This is an alert title')
-                .textContent('You can specify some description text in here.')
-                .ariaLabel('Alert Dialog Demo')
-                .ok('Got it!')
-                .targetEvent(ev)
-        );
-    };
 
     $scope.showAdvanced = function(ev) {
         var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
@@ -87,11 +49,6 @@ app.config(function($routeProvider, $locationProvider, $mdThemingProvider){
                 targetEvent: ev,
                 clickOutsideToClose:true,
                 fullscreen: useFullScreen
-            })
-            .then(function(answer) {
-                $scope.status = 'You said the information was "' + answer + '".';
-            }, function() {
-                $scope.status = 'You cancelled the dialog.';
             });
         $scope.$watch(function() {
             return $mdMedia('xs') || $mdMedia('sm');
@@ -102,15 +59,19 @@ app.config(function($routeProvider, $locationProvider, $mdThemingProvider){
 
 }]);
 
-function DialogController($scope, $mdDialog) {
-    $scope.hide = function() {
+function DialogController($scope, $mdDialog, $http, $location) {
+    $scope.login = function() {
+        $http.post('/login/authenticate', $scope.form)
+            .then(function(response){
+                if(response.data === true){
+                    $location.path('/admin');
+                }
+                else{$location.path('/default');}
+            });
         $mdDialog.hide();
     };
     $scope.cancel = function() {
         $mdDialog.cancel();
-    };
-    $scope.answer = function(answer) {
-        $mdDialog.hide(answer);
     };
 };app.controller('adminCtrl',['$scope','$rootScope', '$http', function($scope, $rootScope, $http){
 
