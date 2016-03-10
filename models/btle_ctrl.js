2e0f21bbb26a6791b71635572865f9156d66d5ff
alnig441 <alnig441@gmail.com>
2016-03-10 14:41:25 -0600

    models amended

--- models/btle_ctrl.js
+++ models/btle_ctrl.js
@@ -5,7 +5,7 @@ var client = new pg.Client(connectionString);
 client.connect();
 var query = client.query('CREATE TABLE devices(id SERIAL PRIMARY KEY, mac TEXT UNIQUE NOT NULL, location VARCHAR(20), device_On BOOLEAN)');
 var query = client.query('CREATE TABLE users(id SERIAL PRIMARY KEY, username VARCHAR(10) UNIQUE NOT NULL, password VARCHAR(100) NOT NULL)');
-var query = client.query('CREATE TABLE profiles(profile_name VARCHAR(25) PRIMARY KEY, turn_on BOOLEAN, colour TEXT, hour INT, minute INT, recur_weekly BOOLEAN, recur_daily BOOLEAN, active BOOLEAN)');
+var query = client.query('CREATE TABLE profiles(profile_name VARCHAR(25) PRIMARY KEY, turn_on BOOLEAN, colour TEXT, hour INT, minute INT, recur_weekly BOOLEAN, recur_daily BOOLEAN, active BOOLEAN, sunrise BOOLEAN, sunset BOOLEAN)');
 var query = client.query('CREATE TABLE connectedProfiles(id TEXT PRIMARY KEY NOT NULL)');
 
 query.on('end', function(){
